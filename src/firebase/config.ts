5eea0811d31d03da8b932798a5ba9901ccfb0aee
CrabsterTechnology <crabstertechnology@gmail.com>
2026-03-19 13:45:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5418824271-71a21",
+  "appId": "1:337798796195:web:91b12a0ea1fce3e3b64016",
+  "apiKey": "AIzaSyAdVdjbrf8KO5TqEx6JsMxqkD_SaR3Sqdk",
+  "authDomain": "studio-5418824271-71a21.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "337798796195"
+};
